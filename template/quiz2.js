5ce34efec44b7e8aa65147e056acbb47b4ed884a
Tyler Kim <tak90@pitt.edu>
2016-03-28 20:24:04 -0400

    All done

--- template/quiz2.js
+++ template/quiz2.js
@@ -1,39 +1,86 @@
-(function($){
+(function(){
 	// This is where you will write your function for the programming challenge
 	// Do not commit console.log statements
 	// Use ajax to reach the api endpoint
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
 	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	$click     = $('.click');
+	$submit    = $('.submit');
+	var dataStuff;
+	var numChanges = 0;
+	var lastword = "";
 
-	$mouseover.on('mouseover', function() {
-		$this = $(this);
+	$mouseover.mouseover(function() {
 		$(this).html('Scrooge McDuck!');
 		$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+	$click.click(function() {
+		$(this).html('Peace Out!');
+		jQuery(this).fadeOut(1500);
 		return false;
 	});
 
-	$submit.on('submit', function(e) {
+	$('#mainform').on('submit', function(e) {
 		e.preventDefault();
-		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+		
+		if ($(this).find('input.thing').val() !== '') {
+			$(this).find('input').each(function() {
+				jQuery(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).append('<h2>Congratulations! You\'ve entered some text!</h2>');
 		}
 	});
 
-	$(document).on(ready, function() {
-		setTimeout({
-			$timeout.fadeIn('slow');
+	$(document).on('ready', function() {
+		$(".timeoutclass").hide();
+		setTimeout(function(){
+			$(".timeoutclass").fadeIn('slow');
 		}, 1000);
+
+		if(document.cookie){
+			lastword = document.cookie;
+			$('#output').html(lastword);
+			document.cookie = "lastword=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+		}
 	});
+
+	$(document).on("click", "#keepbtn", function(){
+		document.cookie="Last time you got: "+lastword;
+	});
+
+	$("#button").click(function(){
+
+		var jqxhr = $.ajax({
+			dataType: "json",
+			url: "http://www.mattbowytz.com/simple_api.json?data=quizData",
+			error: function(){
+				console.log("ERROR");
+			},
+			success: function(data){
+				dataStuff = data['data'];
+				var index = Math.floor(Math.random() * dataStuff.length);
+
+				$('#output').html(dataStuff[index]);
+				lastword = dataStuff[index];
+				$('#button').text("Change It");
+			},
+			type: 'GET'
+		});
+		numChanges+=1;
+		if(numChanges == 1){
+			var btn = document.createElement("BUTTON");
+			btn.setAttribute('id', 'keepbtn');
+			btn.setAttribute('type', 'button');
+			var t = document.createTextNode("Keep It");
+			btn.appendChild(t);
+			var location = document.getElementById("section2");
+			location.appendChild(btn);
+		}
+	});
+
+	
+
 
 })(jQuery);
